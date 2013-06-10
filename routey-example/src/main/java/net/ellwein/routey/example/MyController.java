f2c20b29f3355866c7eec3ca3d293a174dd3bb9c
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 23:54:05 +0200

    added example with mapping and injecting parameters

--- routey-example/src/main/java/net/ellwein/routey/example/MyController.java
+++ routey-example/src/main/java/net/ellwein/routey/example/MyController.java
@@ -1,17 +1,22 @@
 package net.ellwein.routey.example;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import net.ellwein.routey.annotations.Mapping;
+import net.ellwein.routey.annotations.RequestMethod;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class MyController {
 
-	private static final transient Logger LOGGER = LoggerFactory
-			.getLogger(MyController.class);
+	private static final transient Logger LOGGER = LoggerFactory.getLogger(MyController.class);
 
-	@Mapping
-	public void index() {
+	@Mapping(value = { "/.*" }, method = { RequestMethod.GET, RequestMethod.POST })
+	public void index(final HttpServletRequest request, final HttpServletResponse response) {
 		LOGGER.info("index()");
+		LOGGER.info("Request: " + request);
+		LOGGER.info("Response: " + response);
 	}
 }
