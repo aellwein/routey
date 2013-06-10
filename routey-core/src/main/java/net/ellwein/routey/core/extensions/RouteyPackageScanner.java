9489fb526bd8a1b11b44d67a0528ae8f4f60f6f2
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 22:34:03 +0200

    added javadoc

--- routey-core/src/main/java/net/ellwein/routey/core/extensions/RouteyPackageScanner.java
+++ routey-core/src/main/java/net/ellwein/routey/core/extensions/RouteyPackageScanner.java
@@ -2,7 +2,20 @@ package net.ellwein.routey.core.extensions;
 
 import java.util.Collection;
 
+/**
+ * Base Routey extension which provides the name of packages to scan for
+ * annotations.
+ * 
+ * @author Alexander Ellwein
+ * @since 1.0.0
+ */
 public interface RouteyPackageScanner {
 
+	/**
+	 * Adds a collection of package names to scan. NOTE: regular expressions are
+	 * not supported here (yet).
+	 * 
+	 * @return Collection of full-qualified package names.
+	 */
 	Collection<String> addPackagesToScan();
 }
