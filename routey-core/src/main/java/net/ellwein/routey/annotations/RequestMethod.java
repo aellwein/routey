7df792506ed6c6678c0be48a3b210b213418db74
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 22:01:43 +0200

    added javadoc

--- routey-core/src/main/java/net/ellwein/routey/annotations/RequestMethod.java
+++ routey-core/src/main/java/net/ellwein/routey/annotations/RequestMethod.java
@@ -1,5 +1,11 @@
 package net.ellwein.routey.annotations;
 
+/**
+ * Request methods which can be routed with Routey servlet.
+ * 
+ * @author Alexander Ellwein
+ * @since 1.0.0
+ */
 public enum RequestMethod {
 	GET, POST, PUT, DELETE, OPTIONS, HEAD, TRACE
 }
