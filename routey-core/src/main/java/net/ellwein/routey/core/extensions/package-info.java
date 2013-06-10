db4e8f3b4450d6fe3bea1e13b060dcd1858187f7
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 22:38:54 +0200

    added javadoc

--- routey-core/src/main/java/net/ellwein/routey/core/extensions/package-info.java
+++ routey-core/src/main/java/net/ellwein/routey/core/extensions/package-info.java
@@ -1,8 +1,9 @@
 /**
+ * Routey servlet dispatcher can be extended by adding SPI implementations to the interfaces in this package. 
+ * One of the example base extensions is the {@link net.ellwein.routey.core.extensions.RouteyPackageScanner} - 
+ * it is the user app's pointer to the packages which are containing annotations processed by Routey. 
  * 
- */
-/**
- * @author alex
- *
+ * @author Alexander Ellwein
+ * @since 1.0.0
  */
 package net.ellwein.routey.core.extensions;
