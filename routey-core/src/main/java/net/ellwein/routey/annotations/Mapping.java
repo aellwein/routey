8b6de524d05f18450a577f2f424f956b40039588
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 21:57:05 +0200

    added javadoc

--- routey-core/src/main/java/net/ellwein/routey/annotations/Mapping.java
+++ routey-core/src/main/java/net/ellwein/routey/annotations/Mapping.java
@@ -5,17 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Mapping annotation can be used within a user class to annotate a method which
+ * is responsible for request processing.
+ * 
+ * @author Alexander Ellwein
+ * @since 1.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Mapping {
 
 	/**
-	 * Request URIs.
+	 * Default value which may contain request URI(s) in regex form. Routey
+	 * servlet will map to the first matching URI regex in the internal map.
 	 * 
-	 * @return
+	 * @return array of request URIs
 	 */
 	String[] value() default {};
 
+	/**
+	 * Contains the request methods, which are processed on this mapping. On
+	 * default, only GET method requests get processed.
+	 * 
+	 * @return array with request methods
+	 */
 	RequestMethod[] method() default {};
 
 }
