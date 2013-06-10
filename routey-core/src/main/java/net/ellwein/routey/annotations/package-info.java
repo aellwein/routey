d7cd5581bbc5d249fd9b784347fc74da8312e3f9
Alexander Ellwein <alex.ellwein@gmail.com>
2013-06-10 22:40:08 +0200

    added javadoc

--- routey-core/src/main/java/net/ellwein/routey/annotations/package-info.java
+++ routey-core/src/main/java/net/ellwein/routey/annotations/package-info.java
@@ -1,8 +1,7 @@
 /**
- * 
- */
-/**
- * @author alex
+ * This package contains annotations recognized by Routey servlet dispatcher.
  *
+ * @author Alexander Ellwein
+ * @since 1.0.0
  */
 package net.ellwein.routey.annotations;
